bdd6bd520cc51de7d60b9cd8dd05149bb50473b1
Weder Ribas <me@wederribas.com>
2025-02-08 23:15:49 -0800

    Add SNS client

---
+++ hummingbird/src/clients/sns.js
@@ -0,0 +1,27 @@
+import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
+
+/**
+ * Publishes an event to an SNS topic.
+ * @param {object} param0 Function parameters
+ * @param {string} param0.topicArn The ARN of the SNS topic to publish to
+ * @param {object} param0.message The message to publish
+ * @return {Promise<void>}
+ */
+export const publishEvent = async ({ topicArn, message }) => {
+  try {
+    const client = new SNSClient({
+      endpoint: 'http://sns.localhost.localstack.cloud:4566',
+      region: 'ca-central-1',
+    });
+
+    const command = new PublishCommand({
+      TopicArn: topicArn,
+      Message: JSON.stringify(message),
+    });
+
+    await client.send(command);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
